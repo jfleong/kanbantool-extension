2682a99faef7b8492d9910cc9cfc364dc6ccce29
Jason Leong <jason@nerdwallet.com>
2014-04-29 23:24:28 -0700

    started the construction of a graph for the workflow options
    my plan is to construct a graph then do a depth first traversal through that to get the workflow_options in the order that we want them

--- content.js
+++ content.js
@@ -5,84 +5,31 @@ chrome.runtime.sendMessage({
     subject: "showPageAction"
 });
 
-///////////////////////////////////////////////////////////////////////////
-// This method currently will get the workflow options out of order because
-// of the way that the kanban board can be constructed
-//
-// it gets all the leafs in order which will be a problem if you have nested
-// workflow options
-///////////////////////////////////////////////////////////////////////////
-/* Listen for message from the popup */
-chrome.runtime.onMessage.addListener(function(msg, sender, response) {
-    /* First, validate the message's structure */
-    if (msg.from && (msg.from === "popup")
-            && msg.subject && (msg.subject === "DOMInfo")) {
-        var workflow_options = {};
-        var leaves = document.getElementsByClassName('leaf');
-        for (i = 0; i < leaves.length; i += 1 ) {
-            leaf = leaves[i];
-            id_string = leaf.getAttribute('id');
-            workflow_id = id_string.substring(0, id_string.length - 3); 
-            columnName = leaf.getElementsByClassName('hide_when_collapsed')[0].firstElementChild.innerHTML;
-            // remove the extra div at the end
-            columnName = columnName.replace(/<div(.*?)<\/div>/gi, '');
-            // remove the hyphen and spaces in the front
-            columnName = columnName.replace(/-/gi,'').trim();
-            workflow_options[columnName] = workflow_id;
-            // alert(columnName + ' - ' + workflow_id);
-        }
-        var swimlane_options = {};
-        var swimlanes = document.getElementsByClassName('swimlane_header');
-        for (i = 0; i < swimlanes.length; i += 1 ) {
-            swimlane = swimlanes[i];
-            id_string = swimlane.getAttribute('id');
-            swimlane_id = id_string.substring(0, id_string.length - 3); 
-            swimlane_name = swimlane.firstElementChild.getAttribute('title');
-            swimlane_options[swimlane_name] = swimlane_id;
-        }
-        data = {
-            "workflow_options" : workflow_options,
-            "swimlane_options" : swimlane_options
-        };
-        response(data);
-    }
-});
-
-
-///////////////////////////////////////////////////////////////////////////
-// This is another attempt using not leafs but more traversing
-///////////////////////////////////////////////////////////////////////////
-
+// ///////////////////////////////////////////////////////////////////////////
+// // This method currently will get the workflow options out of order because
+// // of the way that the kanban board can be constructed
+// //
+// // it gets all the leafs in order which will be a problem if you have nested
+// // workflow options
+// ///////////////////////////////////////////////////////////////////////////
 // /* Listen for message from the popup */
 // chrome.runtime.onMessage.addListener(function(msg, sender, response) {
 //     /* First, validate the message's structure */
-//     if (msg.from && (msg.from === "popup") && msg.subject && (msg.subject === "DOMInfo")) {
-//         data = {};
+//     if (msg.from && (msg.from === "popup")
+//             && msg.subject && (msg.subject === "DOMInfo")) {
 //         var workflow_options = {};
-//         var boardHeading = document.getElementById('board_heading');
-//         headingRows = boardHeading.getElementsByTagName('tr');
-//         for (i = 1; i < headingRows.length; i++) {
-//             tr = headingRows[i];
-//             columns = tr.getElementsByTagName('th');
-//             for (j = 0; j < columns.length; j++) {
-//                 th = columns[j];
-//                 if (th.classList.contains('board_handler_cell')) {
-//                     // these are filler cells
-//                     continue; 
-//                 }
-//                 isLeaf = th.classList.contains('leaf');
-//                 if (!isLeaf) {
-//                     // i wanna go down
-
-//                 }
-//                 else {
-//                     id_string = th.getAttribute('id');
-//                     workflow_id = id_string.substring(0, id_string.length - 3); 
-//                     columnName = th.getElementsByClassName('hide_when_collapsed')[0].firstElementChild.innerText;
-//                     console.log(columnName);
-//                     workflow_options[columnName] = workflow_id;
-//                 }
-//             }
+//         var leaves = document.getElementsByClassName('leaf');
+//         for (i = 0; i < leaves.length; i += 1 ) {
+//             leaf = leaves[i];
+//             id_string = leaf.getAttribute('id');
+//             workflow_id = id_string.substring(0, id_string.length - 3); 
+//             columnName = leaf.getElementsByClassName('hide_when_collapsed')[0].firstElementChild.innerHTML;
+//             // remove the extra div at the end
+//             columnName = columnName.replace(/<div(.*?)<\/div>/gi, '');
+//             // remove the hyphen and spaces in the front
+//             columnName = columnName.replace(/-/gi,'').trim();
+//             workflow_options[columnName] = workflow_id;
+//             // alert(columnName + ' - ' + workflow_id);
 //         }
 //         var swimlane_options = {};
 //         var swimlanes = document.getElementsByClassName('swimlane_header');
@@ -100,4 +47,72 @@ chrome.runtime.onMessage.addListener(function(msg, sender, response) {
 //         response(data);
 //     }
 // });
+
+
+///////////////////////////////////////////////////////////////////////////
+// This is another attempt using not leafs but more traversing
+///////////////////////////////////////////////////////////////////////////
+
+/* Listen for message from the popup */
+chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+    /* First, validate the message's structure */
+    if (msg.from && (msg.from === "popup") && msg.subject && (msg.subject === "DOMInfo")) {
+        data = {};
+        var boardHeading = document.getElementById('board_heading');
+        headingRows = boardHeading.getElementsByTagName('tr');
+        // to be constructed then depth first traversed
+        graph = {};
+        leafIdToName = {};
+        for (i = 0; i < headingRows.length; i++) {
+            tr = headingRows[i];
+            columns = tr.getElementsByTagName('th');
+            for (j = 0; j < columns.length; j++) {
+                th = columns[j];
+                if (th.classList.contains('board_handler_cell')) {
+                    // these are filler cells
+                    continue; 
+                }
+                // workflow_state_id_th we only want id
+                idString = th.getAttribute('id');
+                workflowId = id_string.substring(15, idString.length - 3);
+                columnName = '';
+                if (th.getElementsByClassName('hide_when_collapsed')[0]) {
+                    columnName = th.getElementsByClassName('hide_when_collapsed')[0].firstElementChild.innerText.trim();
+                }
+                branches = [];
+                var classList = th.className.split(/\s+/);
+                currentNode = graph;
+                for (var k = 0; k < classList.length; k++) {
+                    classItem = classList[k];
+                   if (classItem.indexOf('child_of') > -1) {
+                        id = classItem.substring(9, classItem.length)
+                        branches.push(id);
+                    }
+                }
+                console.log("parents of " + workflowId + "(" + columnName +") | " + branches.toString())
+                // if leaf keep track of it
+                isLeaf = th.classList.contains('leaf');
+                if (isLeaf) {
+                    columnName = th.getElementsByClassName('hide_when_collapsed')[0].firstElementChild.innerText;
+                    leafIdToName[id_string] = columnName;
+                }
+            }
+        }
+        console.log(graph);
+        var swimlane_options = {};
+        var swimlanes = document.getElementsByClassName('swimlane_header');
+        for (i = 0; i < swimlanes.length; i += 1 ) {
+            swimlane = swimlanes[i];
+            id_string = swimlane.getAttribute('id');
+            swimlane_id = id_string.substring(0, id_string.length - 3); 
+            swimlane_name = swimlane.firstElementChild.getAttribute('title');
+            swimlane_options[swimlane_name] = swimlane_id;
+        }
+        data = {
+            "workflow_options" : workflow_options,
+            "swimlane_options" : swimlane_options
+        };
+        response(data);
+    }
+});
 
